2d936587053493dde97e2d7ebf0154c351167c2f
Mahesh Rajendra Bahir <107680212+MaheshB45@users.noreply.github.com>
2023-09-21 17:32:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,45 @@ const inputs = document.querySelectorAll('.controls input');
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+const video = document.querySelector('video');
+const playButton = document.querySelector('.player__button');
+const progressBar = document.querySelector('.progress');
+const progressFilled = document.querySelector('.progress__filled');
+const volumeSlider = document.querySelector('[name="volume"]');
+const speedSlider = document.querySelector('[name="playbackRate"]');
+const skipButtons = document.querySelectorAll('[data-skip]');
+
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+    playButton.textContent = '❚ ❚';
+  } else {
+    video.pause();
+    playButton.textContent = '►';
+  }
+}
+
+function updateProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressFilled.style.flexBasis = `${percent}%`;
+}
+
+function setVolume() {
+  video.volume = volumeSlider.value;
+}
+
+function setPlaybackSpeed() {
+  video.playbackRate = speedSlider.value;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+video.addEventListener('click', togglePlay);
+playButton.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+volumeSlider.addEventListener('input', setVolume);
+speedSlider.addEventListener('input', setPlaybackSpeed);
+skipButtons.forEach(button => button.addEventListener('click', skip));
